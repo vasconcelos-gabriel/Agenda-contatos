b860c6890f27bd60736a1ff37bc3169c97cff7c4
Anna Carolina Coutinho <86803804+annacoutinho@users.noreply.github.com>
2022-08-06 03:58:27 -0300

    Update

--- AgendaContatos.Data/Repositories/UsuarioRepository.cs
+++ AgendaContatos.Data/Repositories/UsuarioRepository.cs
@@ -28,12 +28,42 @@ namespace AgendaContatos.Data.Repositories
                      CONVERT (VARCHAR(32), HASHBYTES('MD5',@Senha), 2),
                       @DataCadastro)
         ";
-            using (var connection = new SqlConnection(SqlServerConfiguration.GetConnectionString()))  
+            using (var connection = new SqlConnection(SqlServerConfiguration.GetConnectionString()))
             {
                 connection.Execute(sql, usuario);
             }
-
         }
 
+        public Usuario GetByEmail(string email)
+        {
+            var sql = @"
+                          SELECT * FROM USUARIO
+                          WHERE EMAIL = @email
+                                   ";
+            using (var connection = new SqlConnection(SqlServerConfiguration.GetConnectionString()))
+            {
+                return connection.Query<Usuario>(sql, new { email })
+                    .FirstOrDefault();
+
+            }
+        }
+                //método para consultar 1 usuário baseado no email
+        public Usuario GetByEmailAndSenha(string email, string senha)
+        {
+            var sql = @"
+                 SELECT * FROM USUARIO
+                 WHERE EMAIL = @email
+                  AND SENHA = CONVERT (VARCHAR(32), HASHBYTES('MD5',@senha), 2)
+               ";
+
+
+            using(var connection = new SqlConnection(SqlServerConfiguration.GetConnectionString()))
+            {
+                return connection.Query<Usuario>(sql, new { email, senha }).FirstOrDefault();
+            }
+
+        }
+            
     }
 }
+
--- AgendaContatos.Mvc/Controllers/AccountController.cs
+++ AgendaContatos.Mvc/Controllers/AccountController.cs
@@ -1,13 +1,48 @@
 ﻿using AgendaContatos.Data.Entites;
 using AgendaContatos.Data.Repositories;
+using AgendaContatos.Mvc.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AgendaContatos.Mvc.Controllers
 {
     public class AccountController : Controller
     {
+        //ROTA: /Account/Login
         public IActionResult Login()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(AccountLoginModel model)
+
+        {
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    //consultar o usuário no banco de dados através do email e da senha
+                    var usuarioRepository = new UsuarioRepository();
+                    var usuario = usuarioRepository.GetByEmailAndSenha(model.Email, model.Senha);
+
+                    if(usuario != null)
+                    {
+                        //redirecionar para outra página
+                        return RedirectToAction("Consulta", "Contatos");
+                    }
+                    else
+                    {
+                        TempData["Mensagem"] = "Acesso negado. Usuário inválido";
+                    }
+                }
+                catch(Exception e)
+                {
+                    TempData["Mensagem"] = $"Falha ao cadastrar: {e.Message}";
+                }
+
+            }
+            
+     
             return View();
         }
 
@@ -16,28 +51,46 @@ namespace AgendaContatos.Mvc.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Register(string nome, string email, string senha, string senhaConfirmacao)
+        public IActionResult Register(AccountRegisterModel model)
 
         {
-            try
+            //verificar se todos os campos trazidos pela classe model
+            //passaram nas regras de validação
+            if(ModelState.IsValid)
             {
-                var usuario = new Usuario();
-                usuario.IdUsuario = Guid.NewGuid();
-                usuario.Nome = nome;
-                usuario.Email = email;
-                usuario.Senha = senha;
-                usuario.DataCadastro = DateTime.Now;
+                try
+                {
+                    var usuarioRepository = new UsuarioRepository();
 
-                var usuarioRepository = new UsuarioRepository();
-                usuarioRepository.Create(usuario);
+                    if (usuarioRepository.GetByEmail(model.Email) != null)
+                    {
+                        TempData["Mensagem"] = $"O email{model.Email} já está cadastrado com outro usuário. Tente outro email.";
 
-                TempData["Mensagem"] = $"Parabéns{usuario.Nome}, sua conta foi cadastrada com sucesso!";
+                    }
+                    else
+                    {
+                        var usuario = new Usuario();
+                        usuario.IdUsuario = Guid.NewGuid();
+                        usuario.Nome = model.Nome;
+                        usuario.Email = model.Email;
+                        usuario.Senha = model.Senha;
+                        usuario.DataCadastro = DateTime.Now;
 
+
+                        usuarioRepository.Create(usuario);
+
+                        TempData["Mensagem"] = $"Parabéns{usuario.Nome}, sua conta foi cadastrada com sucesso!";
+
+                    }
+
+
+                }
+                catch (Exception e)
+                {
+                    TempData["Mensagem"] = $"Falha ao cadastrar {e.Message}";
+                }
             }
-            catch(Exception e)
-            {
-                TempData["Mensagem"] = $"Falha ao cadastrar {e.Message}";
-            }
+ 
             return View();
         }
 
